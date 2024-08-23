8b5ac16c9ab8f26f86f240d2db1df4bbaa60fa40
CanDoesGames <148730255+CanDoesGames@users.noreply.github.com>
2024-08-23 11:28:39 -0500

    Create funfact.bat

---
+++ funfact.bat
@@ -0,0 +1,15 @@
+@echo off
+REM Set the GitHub URL for the .txt file
+set "github_url=https://raw.githubusercontent.com/CanDoesGames/funfact/main/fact.txt"
+
+
+where curl >nul 2>nul
+if %errorlevel%==0 (
+    curl -s %github_url%
+    echo .
+) else (
+    powershell -Command "Invoke-WebRequest -Uri '%github_url%' -UseBasicParsing | Select-Object -ExpandProperty Content"
+    echo .
+)
+
+pause
